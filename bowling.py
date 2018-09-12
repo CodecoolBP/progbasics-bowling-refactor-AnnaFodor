571b387777e2015284f4c6c1b2f98765e1f7a077
Anna Fodor <fodor.ak@gmail.com>
2018-09-12 16:39:00 +0200

    simple solution

--- bowling.py
+++ bowling.py
@@ -1,39 +1,31 @@
 def score(game):
+    rolls = get_rolls(game)
     result = 0
-    frame = 1
-    in_first_half = True
-    last = 0
-    for roll in range(len(game)):
-        result += roll_score(game[roll], get_value(game[roll]), last)
-        if frame < 10 and get_value(game[roll]) == 10:
-            result += get_value(game[roll+1])
-            if game[roll].lower() == 'x':
-                result += roll_score(game[roll+2], get_value(game[roll+2]), get_value(game[roll+1]))
-        last = get_value(game[roll])
-        if in_first_half:
-            in_first_half = not in_first_half
+    roll_id = 0
+    for frame in range(0, 10):
+        if rolls[roll_id] == 10:
+            result += 10 + rolls[roll_id + 1] + rolls[roll_id + 2]
+            roll_id += 1
+        elif rolls[roll_id] + rolls[roll_id + 1] == 10:
+            result += 10 + rolls[roll_id + 2]
+            roll_id += 2
         else:
-            in_first_half = True
-            frame += 1
-        if game[roll].lower() == 'x':
-            in_first_half = True
-            frame += 1
+            result += rolls[roll_id] + rolls[roll_id + 1]
+            roll_id += 2
     return result
 
 
-def get_value(char):
-    if char in "123456789":
-        return int(char)
-    elif char.lower() in "x/":
-        return 10
-    elif char == '-':
-        return 0
-    else:
-        raise ValueError()
-
-
-def roll_score(try_to_check, actual_try_points, last_try_points):
-    if try_to_check == '/':
-        return 10 - last_try_points
-    else:
-        return actual_try_points
+def get_rolls(game):
+    rolls = []
+    for roll in game.lower():
+        if roll == 'x':
+            rolls.append(10)
+        elif roll == '/':
+            rolls.append(10 - rolls[-1])
+        elif roll == '-':
+            rolls.append(0)
+        elif int(roll) > 0 and int(roll) < 10:
+            rolls.append(int(roll))
+        else:
+            raise ValueError()
+    return rolls
